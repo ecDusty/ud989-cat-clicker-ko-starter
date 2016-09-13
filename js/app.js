2e4a7c831ef28c61b4ee25b679609fd339e98f6b
Erin Mulligan <ecmgit@outlook.com>
2016-09-13 15:37:12 +0800

    FEAT: Call for cats Json, added cat list, Can click to set new cat
    FEAT: Call for cats Json, added cat list, Can click to set new cat

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.nameFirst = ko.observable('Tabby');
-  this.nameLast = ko.observable('Macalligan');
+
+function get(url) {
+  return fetch(url);
+}
+
+function getJSON(url) {
+  return get(url).then(function(response) {
+    return response.json();
+  })
+}
+
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.nameFirst = ko.observable(data.nameFirst);
+  this.nameLast = ko.observable(data.nameLast);
   this.fullName = ko.pureComputed(function() {
     return this.nameFirst() + " " + this.nameLast();
   }, this);
 
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.imgAttribution = ko.observable('images url source');
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
 
   this.title = ko.computed(function() {
     var rank = '';
@@ -27,21 +38,79 @@ var Cat = function() {
     return rank;
   }, this)
 
-  this.nickNames = ko.observableArray([
-    { nName: "Mr. Biggles Worth" },
-    { nName: "Tina Fey" },
-    { nName: "T to the F" }
-  ])
+  this.nickNames = ko.observableArray(data.nickNames);
 }
 
 
 var ViewModel = function() {
+  var self = this;
 
-  this.curCat = ko.observable(new Cat());
+  this.catList = ko.observableArray([]);
+  this.curCat = ko.observable();
+
+  getJSON('../json/cats.json')
+  .then(function(response) {
+    response.cats.forEach(function(catItem) {
+      self.catList.push( new Cat(catItem));
+    });
+  }).then(function() {
+    self.curCat(self.catList()[0]);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
   };
+
+  this.setCat = function() {
+    self.curCat(this);
+  }
 }
 
 ko.applyBindings(new ViewModel());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var erin_Mulligan = {
+    email: 'erin@unlikelyit.com',
+    phoneNumber: +852-9283-0998,
+    address: 'Units 1205-9, \
+        12/F, Cyberport 2, \
+        100 Cyberport Road, HK'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
